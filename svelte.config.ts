e9f114d4f2549bd45456d5e95f8772d401b2a632
Sergey <rekenraam@gmail.com>
2026-02-22 15:46:48 +0100

    feat: Add svelte.config.js and update tsconfig.json to include it
    Going back to js to fix Cloudlfare build process at github

--- svelte.config.ts
+++
@@ -1,8 +0,0 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import type { SvelteConfig } from '@sveltejs/vite-plugin-svelte';
-
-const config: SvelteConfig = {
-  preprocess: vitePreprocess(),
-};
-
-export default config;
